4a38f1df8bf289711f2f2cf2d70a80c9b378428c
Luigi Lucas de Carvalho Silva <luigi.lcsilva@gmail.com>
2024-03-11 12:11:24 -0300

    Modifying files.

---
+++ 1_INTRO/hello-world-create/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="acrescentado">
+        <h1>Adicionando um título em React</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
